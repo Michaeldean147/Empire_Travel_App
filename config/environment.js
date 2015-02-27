3beebbf263108542772da47b43fe407bcdff2ea9
Michaeldean147 <michaeldean147@yahoo.com>
2015-02-27 14:43:38 -0800

    prep for heroku deployment

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'starwars',
+    adapterURL: process.env.ADAPTER_URL,
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
